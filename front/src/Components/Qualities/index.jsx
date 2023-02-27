f10e74d327d84ab3434a40edfed952a5d8718183
Noh4m <n_hirep@hetic.eu>
2023-02-27 17:30:19 +0100

    add img with no css

--- front/src/Components/Qualities/index.jsx
+++ front/src/Components/Qualities/index.jsx
@@ -1,23 +1,49 @@
 import React from 'react';
 import useFetch from '../../hooks/fetch'
 import styles from './style.module.css'
+import { useMediaQuery } from 'react-responsive'
 function Qualities() {
-    const {data} = useFetch('http://localhost:1337/api/home?populate[Qualities][populate]=%2A')
-    return (
-        <>
-            <div className={styles.qualities}>
-                {data?.Qualities.Quality.map((qual) => 
-                <div>
-                    <p className={styles.numberQuality}> <span>{qual.numberQuality}</span> </p>  
-                    <p className={styles.titleQuality}> {qual.titleQuality} </p>  
-                    <p className={styles.textQuality}> {qual.textQuality} </p>  
-                </div>
-    
-                )}
-
-            </div>
-        </>
-    );
+	const url = 'http://localhost:1337'
+	const {data} = useFetch('http://localhost:1337/api/home?populate[Qualities][populate]=%2A')
+	const isMobile = useMediaQuery({ maxWidth: 900 })
+	return (
+		<>
+			<div className={styles.qualities}>
+			
+			{isMobile 
+				?data?.Qualities.Quality.map((qual) =>
+					<div className={styles.qualitiesContent}>
+						<p className={styles.numberQuality}> <span>{qual.numberQuality}</span> </p>  
+						<p className={styles.titleQuality}> {qual.titleQuality} </p>  
+						<p className={styles.textQuality}> {qual.textQuality} </p>  
+					</div>
+					)
+				
+					:<div className={styles.containerQualities}>
+						<div className={styles.qualitiesContent}>
+						{data?.Qualities.Quality.map((qual, index) =>
+							<div key={index}>
+								<p className={styles.numberQuality}> <span>{qual.numberQuality}</span> </p>  
+								<p className={styles.titleQuality}> {qual.titleQuality} </p>  
+								<p className={styles.textQuality}> {qual.textQuality} </p>  
+							</div>
+							
+								)}
+								</div>
+						<div className={styles.qualitiesContentImg}>
+							{data?.Qualities.image.data.map((img, index) => 
+							
+								<figure key={index} className={img.attributes.caption}>
+									<img src={url+img.attributes.url} alt="" />
+								</figure>
+							)}
+							
+						</div>
+					</div> 
+			}
+			</div>
+		</>
+	);
 }
 
 export default Qualities;
